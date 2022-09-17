815efd94ec6af4d948c946cfbffa8cb6b939c0a4
kevin keise <kevinkeise@gmail.com>
2022-09-17 18:35:51 -0300

    cards

--- src/components/ProdList.js
+++ src/components/ProdList.js
@@ -1,9 +1,14 @@
 import ProductCard from "./ProductCard";
 import "./ProdList.css";
+import {NavLink} from "react-router-dom"
 
 const ProdList = () => {
     return(
         <div className="container-prod-list">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
             <ProductCard/>
             <ProductCard/>
             <ProductCard/>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,22 +1,25 @@
 import "./ProductCard.css";
+import {NavLink} from "react-router-dom"
 
 const ProductCard = () => {
     return(
         <div className="container-product-card">
-            <div className="container-image-prod-card">
-                <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
-                <div>
-                    <button>Cart</button>
+            <NavLink to="/asd" className="link-card">
+                <div className="container-image-prod-card">
+                    <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
                 </div>
-            </div>
-            <div>
-                <h2>some text</h2>
-            </div>
-            <div>
-                <h2>desc text text</h2>
-            </div>
-            <div>
-                <span>R$ 3,00</span>
+                <div>
+                    <h2>some text</h2>
+                </div>
+                <div>
+                    <h2>desc text text</h2>
+                </div>
+                <div className="container-price-card">
+                    <span>R$ 3,00</span>
+                </div>
+            </NavLink>
+            <div className="container-card-top">
+                <button>Cart</button>
             </div>
         </div>
     );
